334519765809c9fe9a67ecc14fa573fa4161235c
xzjs <2310976219@qq.com>
2016-12-16 15:22:44 +0800

    增加editor配置

---
+++ Assets/Editor/PackBundle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PackBundle : MonoBehaviour {
+	[MenuItem("Bundle/Build")]
+	static void BuildAllAssetBundles()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles");
+	}
+
+	[MenuItem("Bundle/BuildForIOS")]
+	static void BuildAllAssetBundlesForIOS()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles/IOS",BuildAssetBundleOptions.None,BuildTarget.iOS);
+	}
+}
